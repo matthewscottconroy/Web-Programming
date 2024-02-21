83417fd95cf501b230cda111209b36985689986f
Matthew Scott Conroy <matthewconroy@pop-os.localdomain>
2024-02-21 09:16:48 -0500

    Streamlined the templating-with-query-strings-and-files.

--- node-examples/templating-with-query-strings-and-files/routes/index.js
+++ node-examples/templating-with-query-strings-and-files/routes/index.js
@@ -13,20 +13,6 @@ module.exports = (app) => {
 			  //res.json({soda: "Coke", meal: "chicken"});
     });
 
-    app.get('/jabberwocky', (req, res) => {
-        res.render('form');
-			  //res.json({soda: "Coke", meal: "chicken"});
-    });
-
-    // Route to display images
-    app.get('/images', (req, res) => {
-        const imageDir = path.join(__dirname, '../public/images');
-        fs.readdir(imageDir, (err, files) => {
-            if (err) throw err;
-            res.render('images', { images: files });
-        });
-    });
-
 
     app.get('/patient', (req, res) => {
         const patientId = req.query.patientId;
@@ -40,37 +26,6 @@ module.exports = (app) => {
                 .on('end', () => {
                     res.render('patient', { data: results });
                 });     
-    });
-
-    // Route to display data based on query
-    app.get('/data', (req, res) => {
-        const { type, filter } = req.query; // e.g., ?type=json&filter=someValue
-        const filePath = path.join(__dirname, `../public/data/example.${type}`);
-        
-        if (type === 'json') {
-            fs.readFile(filePath, (err, data) => {
-                if (err) throw err;
-                const jsonData = JSON.parse(data).filter(item => item.key === filter);
-                res.render('data', { data: jsonData });
-            });
-        } else if (type === 'csv') {
-            const results = [];
-            fs.createReadStream(filePath)
-                .pipe(csvParser())
-                .on('data', (data) => {
-                    if (data.key === filter) results.push(data);
-                })
-                .on('end', () => {
-                    res.render('data', { data: results });
-                });
-        }
-    });
-
-    // Route to handle form submission
-    app.post('/submit-form', upload.none(), (req, res) => {
-        const { name, email } = req.body;
-        // Process form data here...
-        res.render('form', { name, email });
     });
 };
 
